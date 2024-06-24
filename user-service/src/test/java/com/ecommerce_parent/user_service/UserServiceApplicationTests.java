1744e86df0aa7ea76f44d3f2680e419a45b17af5
ayan <ayan.giri@bigshyft.com>
2024-06-25 00:09:41 +0530

    "unversioned files needed"

---
+++ user-service/src/test/java/com/ecommerce_parent/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_parent.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
